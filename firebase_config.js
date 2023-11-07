b0901c161733988071a2dd15fbb6a5382e1edaf9
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2023-11-07 13:24:00 +0200

    added footer

---
+++ firebase_config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCd56WYJP3nuJW-UmhytuvFaGGcrJjzgdo",
+  authDomain: "wine-and-dine-da5e6.firebaseapp.com",
+  projectId: "wine-and-dine-da5e6",
+  storageBucket: "wine-and-dine-da5e6.appspot.com",
+  messagingSenderId: "969541023097",
+  appId: "1:969541023097:web:d775bf7dbe4dc317422ba8",
+  measurementId: "G-FCDFCFZK01"
+};
+
+const app = initializeApp(firebaseConfig);
+
